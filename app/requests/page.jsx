d98d72f0d1ad3da0f291a91bb888df6faec5b790
Santiago <santiago.riveram@udea.edu.co>
2023-11-21 09:42:12 -0500

    👍

--- app/requests/page.jsx
+++ app/requests/page.jsx
@@ -5,9 +5,11 @@ import "./page.css";
 import { useState, useEffect } from "react";
 
 export default function Requests() {
+
   const [selectedUser, setSelectedUser] = useState(null);
   const [users, setUsers] = useState([]);
   const [isOpen, setIsOpen] = useState(true);
+  const [price, setPrice] = useState(0);
 
   const handleClose = () => {
     setIsOpen(false);
@@ -22,9 +24,37 @@ export default function Requests() {
   const handleUserClick = (user) => {
     setSelectedUser(user);
     setIsOpen(true);
+    setPrice(generateRandomPrice());
+    generateEstimates();
   };
 
+  const generateRandomPrice = () => {
+    return Math.floor(Math.random() * (20000 - 4000 + 1)) + 4000; // Genera un número aleatorio entre 4000 y 20000
+  };
 
+  const [estimatedTime, setEstimatedTime] = useState(0);
+  const [estimatedDistance, setEstimatedDistance] = useState(0);
+
+
+  const generateEstimates = () => {
+    const price = Math.floor(Math.random() * (20000 - 4000 + 1)) + 4000; // Genera un número aleatorio entre 4000 y 20000
+    const distance = price / 1200; // asume que el costo por kilómetro es de 400 COP
+    const time = (distance / 25) * 60; // asume que la velocidad promedio es de 30 km/h
+  
+    setPrice(price);
+    setEstimatedTime(time);
+    setEstimatedDistance(distance);
+  };
+
+  const handleReject = (userToReject) => {
+    setUsers(users.filter(user => user.id !== userToReject.id));
+    handleClose();
+  };
+
+  const handleAccept = (userToAccept) => {
+    setUsers([userToAccept]);
+    handleClose();
+  };
 
   return (
     <div>
@@ -49,25 +79,25 @@ export default function Requests() {
           ))}
         </ul>
         {selectedUser && isOpen && (
-           
           <div>
             {/*<button>messi</button>*/}
             <div className="info">
-            <button id="equis" onClick={handleClose}>X</button>
-            <h1>$0.00</h1>
-            <p>{selectedUser.first_name}</p>
-            <h2>Descripción:</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-              in. Blanditiis, voluptates officiis unde ratione expedita facere,
-              temporibus est veritatis praesentium ea labore adipisci dolorum
-              qui eaque debitis explicabo asperiores.
-            </p>
-            <div className="btns">
-              <button className="acp-btn2">Aceptar</button>
-              <button className="rej-btn2">Rechazar</button>
+              <button id="equis" onClick={handleClose}>
+                X
+              </button>
+              <div className="innerContainer">
+                <h1>${price.toFixed(2)}</h1>
+                <h2>{selectedUser.first_name}</h2>
+                <h2>Descripción:</h2>
+                <p>
+                  Su viaje le tomará aproximadamente {estimatedTime.toFixed(0)} minutos y {estimatedDistance.toFixed(1)} km de distancia.
+                </p>
+                <div className="btns">
+                  <button className="acp-btn2" onClick={() => handleAccept(selectedUser)}>Aceptar</button>
+                  <button className="rej-btn2" onClick={() => handleReject(selectedUser)}>Rechazar</button>
+                </div>
+              </div>
             </div>
-          </div>
           </div>
         )}
       </div>
